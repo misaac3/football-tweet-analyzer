e2c34f41fc79a1f000de41d9bec8698953de8ea7
Ahmad Zaidi <youremail@who.com>
2018-12-12 21:41:37 -0600

    Working algorithm

--- main.m
+++ main.m
@@ -1,17 +1,27 @@
 data = readtable("tweets.csv", "TextType", "string");
 textdata = data.text;
-% document = twitter_analysis(textdata);
+document = twitter_analysis(textdata);
 cleanBag = bagOfWords(document);
 cleanBag = removeInfrequentWords(cleanBag,2);
 [cleanBag,idx] = removeEmptyDocuments(cleanBag);
 
 Y = data.label;
 Y(idx) = [];
-X = cleanBag.Counts;
+X = full(cleanBag.Counts);
 
-Model = fitctree(full(X) ,Y);
+% MdlDefault = fitctree(X,Y,'CrossVal','on');
+Model = TreeBagger(50, X, Y, 'OOBPrediction','on','Method', 'classification');
+oobErrorBaggedEnsemble = oobError(Model);
+figID = figure;
+plot(oobErrorBaggedEnsemble)
+xlabel 'Number of grown trees';
+ylabel 'Out-of-bag classification error';
+print(figID, '-dpdf', sprintf('randomforest_errorplot_%s.pdf', date));
+oobPredict(Model);
+% view trees
+% view(Model.Trees{1},'mode','graph') % graphic description
 
-yeet = twitter_analysis("10 - Atlético de Madrid have drawn ten Nil-nil games in @ChampionsLeague, more than any other team in the competicion since 2013/2014 season, the first for Argentinian in the bench. Padlock");
+yeet = twitter_analysis("Top 3 Most IMPROBABLE Catches of Week 14! http://on.nfl.com/391pnw  @NextGenStats (by @MyStraightTalk)");
 Xt = addDocument(cleanBag, yeet);
 label = predict(Model, full(Xt.Counts(end, 1:cleanBag.NumWords)))
 
