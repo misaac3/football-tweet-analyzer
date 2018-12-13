257e36607c077aae0b86e99888042ed394d0553b
misaac3 <misaac3@wisc.edu>
2018-12-12 20:35:16 -0600

    Python scrapes full tweets

--- TwitterScrape.py
+++ TwitterScrape.py
@@ -29,47 +29,75 @@ def get_all_tweets(screen_name, label):
     # save the id of the oldest tweet less one
     oldest = alltweets[-1].id - 1
     c = 0
-    # keep grabbing tweets until there are no tweets left to grab
-    while len(new_tweets) > 0 and c < 10:
-        c += 1
-        print("getting tweets before %s" % (oldest))
 
-        # all subsiquent requests use the max_id param to prevent duplicates
-        new_tweets = api.user_timeline(screen_name=screen_name, count=200, max_id=oldest)
+    # ---------------------------------------------------------------
+    for status in tweepy.Cursor(api.user_timeline, screen_name=screen_name, count=200, max_id=oldest, tweet_mode="extended").items():
+        # process status here
+        text = status.full_text
 
-        # save most recent tweets
-        alltweets.extend(new_tweets)
+        text = text.split('\n')
+        text = " ".join(text)
 
-        # update the id of the oldest tweet less one
-        oldest = alltweets[-1].id - 1
+        # replace &amp with and
+        text.replace('&amp;', 'and')
+        text = re.sub(r'http\S+', '', text)
 
-        print("...%s tweets downloaded so far" % (len(alltweets)))
-
-        # transform the tweepy tweets into a 2D array that will populate the csv
-        outtweets = [[tweet.text] for tweet in alltweets]
-
-        for line in outtweets:
-            # remove \n
-            text = line[0]
-            text = text.split('\n')
-            line[0] = " ".join(text)
-            line[0] = line[0].strip("…")
-
-            # replace &amp with and
-            line[0].replace('&amp;', 'and')
-            line[0] = re.sub(r'http\S+', '', line[0])
-
-            # write the csv
+        # write the csv
         with open('tweets.csv', 'a', encoding="utf-8") as f:
             writer = csv.writer(f)
-            writer.writerow(["text", "label"])
-            for line in outtweets:
-                writer.writerow([line[0], label])
+            writer.writerow([text, label])
         pass
+        print(text)
+
+    return
+
+
+    # ---------------------------------------------------------------
+    # keep grabbing tweets until there are no tweets left to grab
+    # while len(new_tweets) > 0 and c < 1:
+    #     c += 1
+    #     print("getting tweets before %s" % (oldest))
+    #
+    #     # all subsiquent requests use the max_id param to prevent duplicates
+    #     new_tweets = api.user_timeline(screen_name=screen_name, count=200, max_id=oldest, tweet_mode="extended")
+    #
+    #     # save most recent tweets
+    #     alltweets.extend(new_tweets)
+    #
+    #     # update the id of the oldest tweet less one
+    #     oldest = alltweets[-1].id - 1
+    #
+    #     print("...%s tweets downloaded so far" % (len(alltweets)))
+    #
+    #     # transform the tweepy tweets into a 2D array that will populate the csv
+    #     outtweets = [[tweet.full_text] for tweet in alltweets if hasattr(tweet, "full_text")]
+    #
+    #     for line in outtweets:
+    #         # remove \n
+    #         text = line[0]
+    #         text = text.split('\n')
+    #         line[0] = " ".join(text)
+    #         line[0] = line[0].strip("…")
+    #
+    #         # replace &amp with and
+    #         line[0].replace('&amp;', 'and')
+    #         line[0] = re.sub(r'http\S+', '', line[0])
+    #
+    #         # write the csv
+    #     with open('tweets.csv', 'a', encoding="utf-8") as f:
+    #         writer = csv.writer(f)
+    #         writer.writerow(["text", "label"])
+    #         for line in outtweets:
+    #             writer.writerow([line[0], label])
+    #     pass
 
 
 if __name__ == '__main__':
     # pass in the username of the account you want to download
+    with open('tweets.csv', 'a', encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(["text", "label"])
+    pass
     get_all_tweets("NFLResearch", "0")
     get_all_tweets("OptaJoe", "1")
     print("Done!")
